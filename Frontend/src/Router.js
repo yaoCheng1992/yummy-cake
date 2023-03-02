29034bb2a1517617018f0f879826efe43ca75e02
yaozheng <gloriousfutureserbia@gmail.com>
2023-03-02 15:20:03 +0100

    combine both change

--- Frontend/src/Router.js
+++ Frontend/src/Router.js
@@ -7,7 +7,9 @@ import Header from './Header'
 import Footer from './Footer'
 import Main from './Main'
 import Login from './components/login/Login';
-import Products from './Products'
+import Products from './Products' 
+import CustomerRegistration from './components/Register/Customers/CustomerRegistration';
+import Register from './components/Register/Register';
 const Router = createBrowserRouter([
   {
     element: <>
@@ -29,6 +31,15 @@ const Router = createBrowserRouter([
   {
     path: '/login',
     element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register/>
+  },
+  {
+    path: "/customer-registration",
+    element: <CustomerRegistration/>
   }
+  
 ]);
 export default Router
